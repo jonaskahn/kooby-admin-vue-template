f9590e0f3a6a7014c4b0c4700ccb28a10927fd09
tungntdev <tungntdev@gmail.com>
2024-08-12 20:24:07 +0700

    update missing code

--- backend/src/main/kotlin/io/github/jonaskahn/repositories/impl/UserRepositoryImpl.kt
+++ backend/src/main/kotlin/io/github/jonaskahn/repositories/impl/UserRepositoryImpl.kt
@@ -1,12 +1,14 @@
 package io.github.jonaskahn.repositories
 
 import io.github.jonaskahn.assistant.query.JpaQueryExecutor
+import io.github.jonaskahn.constants.Defaults
 import io.github.jonaskahn.entities.User
 import io.github.jonaskahn.entities.enums.Status
 import io.github.jonaskahn.services.user.UserDto
 import io.jooby.Context
 import jakarta.inject.Inject
 import jakarta.persistence.EntityManager
+import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 
 class UserRepositoryImpl @Inject constructor(
@@ -74,7 +76,25 @@ class UserRepositoryImpl @Inject constructor(
     }
 
     override fun countByKeywordAndStatus(keyword: String?, status: Collection<Int>): Long {
-        TODO("Not yet implemented")
+        return count(true, "select count(1) from users u where 1 = 1 ") { builder, params ->
+            queryBuilderByKeywordAndStatus(keyword, builder, params, status)
+        }
+    }
+
+    private fun queryBuilderByKeywordAndStatus(
+        keyword: String?,
+        builder: StringBuilder,
+        params: MutableMap<String, Any>,
+        status: Collection<Int>
+    ) {
+        if (StringUtils.isNotBlank(keyword)) {
+            builder.append(" AND (u.username like %:keyword% or u.email like %:keyword%)")
+            params["keyword"] = keyword!!
+        }
+        if (status.isNotEmpty()) {
+            builder.append(" AND u.status in (:status)")
+            params["status"] = status
+        }
     }
 
     override fun searchByKeywordAndStatusAndOffset(
@@ -82,7 +102,12 @@ class UserRepositoryImpl @Inject constructor(
         status: Collection<Int>,
         offset: Long
     ): Collection<UserDto> {
-        TODO("Not yet implemented")
+        return search(true, "select * from users u where 1 = 1 ", UserDto::class.java) { builder, params ->
+            queryBuilderByKeywordAndStatus(keyword, builder, params, status)
+            builder.append(" limit :limit offset :offset")
+            params["limit"] = Defaults.Pageable.DEFAULT_PAGE_SIZE
+            params["offset"] = offset
+        }
     }
 
 
